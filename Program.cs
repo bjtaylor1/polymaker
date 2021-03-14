c48ec65a1eeb151e4888ca1b9cde3c37d9361682
Ben Taylor <bentaylortheonly@gmail.com>
2021-03-14 09:35:04 +0000

    make 360 polys

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace polymaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(-180, 180, async (i, ps) => 
+            {
+                await using var sw = new StreamWriter($"degree{i}.poly");
+                await sw.WriteLineAsync($"degree{i}");
+                await sw.WriteLineAsync($"standard");
+                await sw.WriteLineAsync($"  {i} 90");
+                await sw.WriteLineAsync($"  {i} -90");
+                await sw.WriteLineAsync($"  {i+1} -90");
+                await sw.WriteLineAsync($"  {i+1} 90");
+                await sw.WriteLineAsync($"  {i} 90");
+                await sw.WriteLineAsync($"END");
+                await sw.WriteLineAsync($"END");
+            });
+        }
+    }
+}
